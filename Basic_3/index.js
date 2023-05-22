45903b4f7f348b848708f63e1f1d318b1bf0330b
Julian <huangbobeijing2008@gmail.com>
2023-05-22 22:12:33 +1200

    add event and listen to the event to create the logs.txt

--- Basic_3/index.js
+++ Basic_3/index.js
@@ -1,10 +1,28 @@
-const { format } = require('date-fns');
+// const { format } = require('date-fns');
 
-const { v4: uuid } = require('uuid');
+// const { v4: uuid } = require('uuid');
 
-// const uuid = require('uuid');
+// // const uuid = require('uuid');
 
-console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'));
+// console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'));
 
-console.log('ho');
-console.log(uuid());
+// console.log('ho');
+// console.log(uuid());
+
+/**
+ * create custom event class and listen to the custom 'log' event,and use setTimeout to imitate the asynchronous
+ * function
+ */
+const logEvents = require('./logEvents');
+
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter {}
+
+const myEmitter = new MyEmitter();
+
+myEmitter.on('log', (msg) => logEvents(msg));
+
+setTimeout(() => {
+  myEmitter.emit('log', 'log event emitted');
+}, 2000);
