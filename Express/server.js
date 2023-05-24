a13e4dd9995f9cb632b4b370349c9340ad892900
Julian <huangbobeijing2008@gmail.com>
2023-05-24 12:58:36 +1200

    to use the express to create a server and set up some routers to access to some pages

---
+++ Express/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3500;
+
+//get router
+app.get('^/$|index(.html)?', (req, res) => {
+  //this way can access the index.html, but cannot show the picture directly
+  // res.sendFile('./views/index.html', { root: __dirname });
+
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  // res.send('Hello world');
+});
+
+app.get('/new(.html)?', (req, res) => {
+  // console.log(__dirname);
+  res.sendFile(path.join(__dirname, 'views', 'new.html'));
+});
+
+//because of the cached data, i visited new-page.html by using old url, so it could cause some potential problem if I don't clean the cached data
+app.get('/old(.html)?', (req, res) => {
+  res.redirect(301, '/new.html');
+});
+
+//if all url cannot access to any pages, then return 404page, which is default
+//but this place should be noticed because the app succeed to response a page named 404, so the default status code is 200, which is not we expect, it should be 404 status code, therefore we need to set the status code to 404
+app.get('/*', (req, res) => {
+  // res.sendFile(path.join(__dirname, 'views', '404.html'));
+  res.statusCode(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+//start the server and make a listen for incoming network connections on a specified port
+app.listen(PORT, () => console.log('Server running on ' + `${PORT}`));
